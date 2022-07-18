516c31245e4fd520733a0e733ea69bd76726d247
gary <gary@MACSTATION.local>
2022-07-18 18:41:28 +0100

    Updates

---
+++ deployment/kubernetes-local/init-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+####################################################################
+# A script used by the init job to wait until Elasticsearch is ready
+####################################################################
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
---
+++ deployment/kubernetes-local/wait-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+################################################################
+# A script used by the init job to initialize Elasticsearch data
+################################################################
+
+cd "$(dirname "${BASH_SOURCE[0]}")"
