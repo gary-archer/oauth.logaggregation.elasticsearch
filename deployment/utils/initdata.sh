471414768e5114f8845fbed25bda254a39f62fa0
gary <gary@MACSTATION.local>
2022-07-19 18:27:02 +0100

    Updates

--- deployment/utils/initdata.sh
+++ deployment/utils/initdata.sh
@@ -73,8 +73,6 @@ fi
 # Create the Elasticsearch schema for apilogs
 #
 echo 'Creating the Elasticsearch ingestion pipeline ...'
-echo '*** DEBUG INGESTION'
-cat "$INGESTION_PIPELINE_FILE_PATH"
 HTTP_STATUS=$(curl -k -s -X PUT "$ELASTIC_URL/_ingest/pipeline/apilogs" \
 -u "$ELASTIC_USER:$ELASTIC_PASSWORD" \
 -H "content-type: application/json" \
